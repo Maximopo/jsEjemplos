985b8251db24da9e22eabd616546f00b7c4f1e4a
Máximo <aranguezmaximo@gmail.com>
2024-03-25 16:56:20 -0300

    Create Ejercicio6.js

---
+++ Ejercicio6.js
@@ -0,0 +1,19 @@
+function ejercicio6(params){
+
+    let parámetro = prompt("Ingrese el número entero");
+    let resul = paroImpar(parámetro);
+
+    console.log(resul);
+}
+
+function paroImpar(num) {
+    if (num == 0){
+        return "El número ingresado no es válido";
+    }
+    if (num % 2 == 0 ) {
+    return "El numero es par";
+    }
+     else {
+    return "El numero es impar";
+    }
+ }
