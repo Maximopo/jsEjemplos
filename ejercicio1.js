77e290dafb6a9d743ee9fa79b9205b4b6658264f
Máximo <aranguezmaximo@gmail.com>
2024-03-25 14:41:16 -0300

    Ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,34 @@
+function ejercicio1(){
+    let meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octumbre","Noviembre","Diciembre"];
+    for (let i = 0; i < meses.length; i++){
+        console.log(meses[i]);
+    }
+    
+}
+ function ejercicio2(){
+    var valores = [true,5,false,"hola","adios",2];
+
+    if(valores [3].length < valores [4].length){
+        console.log("adios es mayor que hola");
+        console.log(valores[0]);
+    }
+    else{
+        console.log("hola no es mayor que adios");
+        console.log[2];
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
