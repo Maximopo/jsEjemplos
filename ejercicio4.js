92cbd8f75f981491109325e19bc7312ed33a2abc
Máximo <aranguezmaximo@gmail.com>
2024-03-25 14:41:57 -0300

    Ejercicio 4 js

---
+++ ejercicio4.js
@@ -0,0 +1,11 @@
+function ejercicio4(){
+    var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
+    'E', 'T'];
+    var DNI = prompt ("el mensaje");
+    var posicion= DNI/23;
+
+    if (DNI<0 && DNI > 99999999){
+        DNI = prompt ("el número proporcionado no es válido");
+
+    }
+}
