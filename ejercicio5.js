b7f02c9902f153b96e3afaf476ba1f43022fe571
Máximo <aranguezmaximo@gmail.com>
2024-03-25 15:18:30 -0300

    Update ejercicio5.js

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,4 +1,4 @@
-function ejercicio4(){
+function ejercicio5(){
     var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K',
     'E', 'T'];
     var DNI = prompt ("el mensaje");
