c9f1f1b2685e8a6587e80424b4516059470f6835
Máximo <aranguezmaximo@gmail.com>
2024-03-25 14:41:31 -0300

    Ejercicio 2 js

---
+++ ejercicio2.js
@@ -0,0 +1,16 @@
+function ejercicio2(){
+    var valores = [true,5,false,"hola","adios",2];
+
+    if(valores [3].length < valores [4].length){
+        console.log("adios es mayor que hola");
+        console.log(valores[0]);
+    }
+    else{
+        console.log("hola es más chico que adios");
+        console.log[2];
+    }
+    console.log(valores[1]+valores[5]);
+    console.log(valores[1]-valores[5]);
+    console.log(valores[1]*valores[5]);
+    console.log(valores[1]/valores[5]);
+ }
