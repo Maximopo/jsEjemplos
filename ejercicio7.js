e5e599c447cd4e41d0658fd0d909c72d21ba3e91
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-03-27 15:32:32 -0300

    Update ejercicio7.js

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,4 +1,4 @@
-function ejercicio7(MayusMinus){
+function ejercicio7(){
     var  Mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
     'W', 'X', 'Y', 'Z'];
     var  Minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
