8a5865139e07010ccf9a3d8b6e719304b7484304
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-03-27 15:31:27 -0300

    Update ejercicio7.js

--- ejercicio7.js
+++ ejercicio7.js
@@ -1,23 +1,24 @@
-function ejercicio7(text){
+function ejercicio7(MayusMinus){
     var  Mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
     'W', 'X', 'Y', 'Z'];
-    var Minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+    var  Minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
     'w', 'x', 'y', 'z'];
     
-    let cadena = [Mayusculas || Minusculas];
+    var cadena = [Mayusculas || Minusculas];
 
     let text = prompt("Ingrese una cadena de texto");
-    let result = cadena(text);
+    let result = MayusMinus(text);
 
     console.log(result);
-
-    if (cadena == var [Mayusculas]) { 
-        cadena = prompt ("Esta cadena de textos esta formada por mayusculas");
+}
+function MayusMinus(cadena){
+    if (cadena == Mayusculas) { 
+        return "Esta cadena de textos esta formada por mayusculas"
     }
-    else if (cadena == var [Minusculas] ) {
-        cadena = prompt ("Esta cadena de textos esta formada por minusculas");
+    else if (cadena == Minusculas ) {
+        return "Esta cadena de textos esta formada por minusculas"
     }
      else {
-        cadena = prompt ("Esta cadena de textos esta formada por minusculas y Mayusculas");
+        return "Esta cadena de textos esta formada por minusculas y Mayusculas"
     }
  }
