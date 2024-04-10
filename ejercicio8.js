ea31d7c0947374e81dcecdb25e96dc17442201c1
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-04-10 13:54:01 -0300

    Update ejercicio8.js

--- ejercicio8.js
+++ ejercicio8.js
@@ -1,24 +1,18 @@
-function ejercicio7(){
-    var  Mayusculas = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
-    'W', 'X', 'Y', 'Z'];
-    var  Minusculas = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-    'w', 'x', 'y', 'z'];   
-
-    let text = prompt("Ingrese una cadena de texto");
-    let resul = MayusMinus(text);
-
-    console.log(resul);
+function ejercicio8(){
+    
+    function palindromo(cadena) {
+        
+        cadena = cadena.toLowerCase().replace(/ /g, '');
+    
+        
+        return cadena === cadena.split('').reverse().join('');
+    }
+    
+    
+    var texto = "La ruta nos aporto otro paso natural";
+    if (palindromo(texto)) {
+        console.log("La cadena es un palíndromo.");
+    } else {
+        console.log("La cadena no es un palíndromo.");
+    }
 }
-
-function MayusMinus(cadena) {
-
-    if (cadena == Mayusculas) { 
-        return "Esta cadena de textos esta formada por mayusculas"
-    }
-    else if (cadena == minusculas ) {
-        return "Esta cadena de textos esta formada por minusculas"
-    }
-     else {
-        return "Esta cadena de textos esta formada por minusculas y Mayusculas"
-    }
- }
