196ea202f97f254467a9b9b2e1d1348545afc814
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-04-10 14:19:06 -0300

    Update ejercicio7.js

--- ejercicio7.js
+++ ejercicio7.js
@@ -4,12 +4,12 @@ function ejercicio7(){
     let txt =prompt("Ingrese texto")
     
     let result=mayusmin(txt);
+    let may = 0;
+    let min = 0;
 
     console.log (result);
  }
     function mayusmin(txt){
-    let may = 0;
-    let min = 0;
         
     for (let i = 0; i < txt.length; i++) {
     if (txt.charAt(i)== txt.charAt(i).toUpperCase()) {
