0c0ad003274e513d9ad6087941fce95cbf5e271c
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-04-10 03:14:55 -0300

    Update ejercicio10.js

--- ejercicio10.js
+++ ejercicio10.js
@@ -1,20 +1,34 @@
 function ejercicio10(){
     
-    let text = prompt("Ingrese una cadena de texto");
+    let numeros = prompt("Ingrese una cadena de texto");
     let resul = MayusMinus(text);
 
     console.log(resul);
 }
+function simularLanzamiento() {
+    let dado1 = Math.floor(Math.random() * 6) + 1;
+    let dado2 = Math.floor(Math.random() * 6) + 1;
+    
+    
+    let suma = dado1 + dado2;
+    return suma;
+}
 
-function MayusMinus(cadena) {
+function contarApariciones() {
+    let resultados = Array(11).fill(0);
 
-    if (cadena == Mayusculas) { 
-        return "Esta cadena de textos esta formada por mayusculas"
+    
+    for (let i = 0; i < 36000; i++) {
+        let suma = simularLanzamiento();
+        resultados[suma - 2]++; 
     }
-    else if (cadena == minusculas ) {
-        return "Esta cadena de textos esta formada por minusculas"
-    }
-     else {
-        return "Esta cadena de textos esta formada por minusculas y Mayusculas"
-    }
- }
+
+    return resultados;
+}
+
+let resultados = contarApariciones();
+
+console.log("Número de apariciones de cada suma de los dados:");
+for (let i = 0; i < resultados.length; i++) {
+    console.log(`Suma ${i + 2}: ${resultados[i]} apariciones`);
+}
