a6a7979fac6585d48f7d45169627b6ff1b1be5c5
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-04-10 14:14:17 -0300

    Update ejercicio8.js

--- ejercicio8.js
+++ ejercicio8.js
@@ -2,17 +2,18 @@ function ejercicio8(){
     
     function palindromo(cadena) {
         
-        cadena = cadena.toLowerCase().replace(/ /g, '');
-    
-        
-        return cadena === cadena.split('').reverse().join('');
+        const cadenaProcesada = cadena.toLowerCase().replace(/[^a-z0-9]/g, '');
+        const cadenaInvertida = cadenaProcesada.split('').reverse().join('');
+        return cadenaProcesada === cadenaInvertida;
     }
     
+    const texto = prompt("Ingrese una cadena de texto para verificar si es un palíndromo:");
+    const resultado = palindromo(texto);
     
-    var texto = "La ruta nos aporto otro paso natural";
-    if (palindromo(texto)) {
+    if (resultado) {
         console.log("La cadena es un palíndromo.");
     } else {
         console.log("La cadena no es un palíndromo.");
     }
+    
 }
