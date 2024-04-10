8168732f362b4312587a694f49db7719fcf7fa5e
Maximopo <164915174+Maximopo@users.noreply.github.com>
2024-04-10 03:41:22 -0300

    Update ejercicio8.js

--- ejercicio8.js
+++ ejercicio8.js
@@ -1,19 +1,17 @@
 function ejercicio8(){
-    
     function esPalindromo(cadena) {
+        // Convertir la cadena a minúsculas y eliminar los espacios en blanco
+        cadena = cadena.toLowerCase().replace(/\s/g, '');
         
-        cadena = cadena.toLowerCase().replace(/ /g, '');
-    
-        
+        // Comparar la cadena original con su reverso
         return cadena === cadena.split('').reverse().join('');
     }
     
-    
-    var texto = "La ruta nos aporto otro paso natural";
+    // Ejemplo de uso:
+    let texto = prompt("Ingresa una cadena de texto para verificar si es un palíndromo:");
     if (esPalindromo(texto)) {
         console.log("La cadena es un palíndromo.");
     } else {
         console.log("La cadena no es un palíndromo.");
     }
 }
-
